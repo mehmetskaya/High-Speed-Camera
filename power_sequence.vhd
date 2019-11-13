e6aa6270e6c5eaceef32ac4d8280f983d74205c8
Mehmet S Kaya <msirackaya@gmail.com>
2019-11-13 14:34:19 -0700

    Update power_sequence.vhd

--- power_sequence.vhd
+++ power_sequence.vhd
@@ -1,37 +1,21 @@
-----------------------------------------------------------------------------------
--- Company: Pollution Control Technologies
--- Engineer: Mehmet S KAYA
--- 
--- Create Date: 06/20/2018 01:02:01 PM
--- Design Name: Power Sequence
--- Module Name: power_sequence - Behavioral
--- Project Name: High Speed Camera
--- Target Devices: ZedBoard
--- Tool Versions: Vivado 2016.4
--- Description: Power Sequence of the image sensor
--- 
--- Dependencies: 
--- 
--- Revision: 
--- Revision 0.00 - File Created
--- Additional Comments: 
--- 
-----------------------------------------------------------------------------------
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
 
 entity power_sequence is
-    Port ( clk_in : in STD_LOGIC;
-           sw : in STD_LOGIC;
-           C5514_enable : out STD_LOGIC;
-           Enable_VDD_18 : out STD_LOGIC;
-           Enable_VDD_33 : out STD_LOGIC;
+
+Port (     clk_in         : in STD_LOGIC;
+           sw             : in STD_LOGIC;
+           C5514_enable   : out STD_LOGIC;
+           Enable_VDD_18  : out STD_LOGIC;
+           Enable_VDD_33  : out STD_LOGIC;
            Enable_VDD_pix : out STD_LOGIC;
-           clk_en : out STD_LOGIC;
-           reset_en : out STD_LOGIC;
-           spi_upload : out STD_LOGIC;
-           out_StopCount : out STD_LOGIC);
+           clk_en         : out STD_LOGIC;
+           reset_en       : out STD_LOGIC;
+           spi_upload     : out STD_LOGIC;
+           out_StopCount  : out STD_LOGIC
+		 );
+		 
 end power_sequence;
 
 architecture Behavioral of power_sequence is
@@ -40,34 +24,68 @@ signal sw_in : STD_LOGIC;
 signal counter : integer :=0;
 signal StopCount :std_logic := '1';
 
+type state_type is(sleep,awake,power_up_seq,power_on,power_down_seq,power_down);
+signal state: state_type;
+attribute INIT: STRING;
+attribute INIT OF state: SIGNAL IS "sleep";
+
 begin
 
 sw_in <= sw;
 out_StopCount <= StopCount;
- 
-process  (clk_in,StopCount,sw_in)
 
-begin 
+process (clk_in, sw_in)
+
+begin
+
+  if sw_in = '0' and StopCount = '1' then
+     state <= awake;
+	             C5514_enable   <= '0';
+                Enable_VDD_18  <= '0';
+                Enable_VDD_33  <= '0';
+                Enable_VDD_pix <= '0';
+                clk_en         <= '0';
+                reset_en       <= '0';
+                spi_upload     <= '0';
+	  
+  elsif clk_in'event and clk_in = '1' then
     
-    if falling_edge(sw_in) then
-    
-    StopCount <= '0';
-    counter <= 0;
-    
-    elsif rising_edge(sw_in) then
-        
-    StopCount <= '1';
-    counter <= 0;
-    
-    end if;
-    
-    if rising_edge(clk_in) then    
-                
-                if sw_in = '1' and StopCount = '1' then
-                      
-                counter <= counter + 1;
-                --every 2000000 count is 20ms wait time on 10ns clock           
-                           case counter is
+	 case state is
+	 
+	    when awake =>
+           if sw_in = '1' then
+              state <= power_up_seq;
+				  StopCount <= '0';
+           elsif sw_in = '0' then
+              state <=power_down ;
+           end if;
+	 
+	    when power_down =>
+		      if sw_in = '1' then
+				    state <= power_up_seq;
+	             
+	         elsif sw_in = '0' or sw_in = 'U' then
+				    counter        <= 0;
+                C5514_enable   <= '0';
+                Enable_VDD_18  <= '0';
+                Enable_VDD_33  <= '0';
+                Enable_VDD_pix <= '0';
+                clk_en         <= '0';
+                reset_en       <= '0';
+                spi_upload     <= '0';
+					 state <= power_down;
+				end if;
+				
+       when power_up_seq =>
+           if sw_in = '0' and StopCount = '0' then
+			      state <= power_down_seq;
+					counter <= 0;
+			      StopCount <= '0';
+		
+           else
+               counter <= counter + 1;
+                          
+                      case counter is
                            when 2000000 =>
                                C5514_enable <= '1';
                            when 4000000 =>
@@ -82,16 +100,20 @@ begin
                                reset_en <= '1';
                            when 14000000=>   
                                spi_upload <= '1';
-                               StopCount <= '0';
-                               counter <= 0;
+										 StopCount <= '0';
+										 state <= power_on;
+										 counter <= 0;
                            when others =>
-                           end case;
-
-                elsif sw_in = '0' and StopCount = '0' then                           
-                
-                counter <= counter + 1;
-                --every 2000000 count is 20ms wait time on 10ns clock                                  
-                           case counter is
+                        end case;
+              
+           end if;
+			  
+		 when power_down_seq =>
+           if sw_in = '0' and StopCount = '0' then
+			  
+			     counter <= counter + 1;
+                          
+                      case counter is
                            when 2000000 =>
                                spi_upload <= '0';
                            when 4000000 =>
@@ -106,38 +128,43 @@ begin
                                Enable_VDD_18 <= '0';
                            when 14000000=>   
                                C5514_enable <= '0';
-                               StopCount <= '1';
+										 state <= power_down;
                                counter <= 0;
                           when others =>
-                           end case;
-                
-                elsif sw_in = '1' and StopCount = '0' then
-                
-                counter <= 0;
-                C5514_enable <= '1';
-                Enable_VDD_18 <= '1';
-                Enable_VDD_33 <= '1';
+                       end case;
+           else
+              counter <= 0;
+				  StopCount <= '1';
+              state <= power_up_seq;
+			  end if;  
+				  
+		when power_on =>
+		      if sw_in = '0' or sw_in = 'U'  then
+	             state <= power_down_seq;
+	         else
+				    counter        <= 0;
+                C5514_enable   <= '1';
+                Enable_VDD_18  <= '1';
+                Enable_VDD_33  <= '1';
                 Enable_VDD_pix <= '1';
-                clk_en <= '1';
-                reset_en <= '1';
-                spi_upload <= '1';                
-
-                else
-                counter <= 0;
-                C5514_enable <= '0';
-                Enable_VDD_18 <= '0';
-                Enable_VDD_33 <= '0';
-                Enable_VDD_pix <= '0';
-                clk_en <= '0';
-                reset_en <= '0';
-                spi_upload <= '0';
-                
-                end if;
-   
-        end if;
-        
-
-
+                clk_en         <= '1';
+                reset_en       <= '1';
+                spi_upload     <= '1';
+				    state <= power_on;
+            end if;
+				
+				
+       when others =>
+		      if (sw_in = '0' and StopCount = '1') or (sw_in = '1' and StopCount = '0') then
+	             state <= power_down_seq;
+	         elsif (sw_in = '1' and StopCount = '1') or (sw_in = '0' and StopCount = '0') then
+				    state <= power_up_seq;
+            end if;
+      
+	 end case;
+	 
+  end if;
+  
 end process;
-     
+
 end Behavioral;

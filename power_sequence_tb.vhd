347e18e61986c36ff374bf57025114dfa8176f91
Mehmet S Kaya <msirackaya@gmail.com>
2019-11-16 21:39:41 -0700

    Update power_sequence_tb.vhd

--- power_sequence_tb.vhd
+++ power_sequence_tb.vhd
@@ -1,30 +1,3 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   13:37:54 11/13/2019
--- Design Name:   
--- Module Name:   Y:/Sirac/FPGA_research/power_up/power_sequence_tb.vhd
--- Project Name:  power_up
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: power_up
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
@@ -50,7 +23,7 @@ ARCHITECTURE behavior OF power_sequence_tb IS
          clk_en : OUT  std_logic;
          reset_en : OUT  std_logic;
          spi_upload : OUT  std_logic;
-			state_reg  : out STD_LOGIC_VECTOR ( 2 downto 0);
+	 state_reg  : out STD_LOGIC_VECTOR ( 2 downto 0);
          out_StopCount : OUT  std_logic
         );
     END COMPONENT;
@@ -68,7 +41,7 @@ ARCHITECTURE behavior OF power_sequence_tb IS
    signal clk_en : std_logic;
    signal reset_en : std_logic;
    signal spi_upload : std_logic;
-	signal state_reg :STD_LOGIC_VECTOR ( 2 downto 0);
+   signal state_reg :STD_LOGIC_VECTOR ( 2 downto 0);
    signal out_StopCount : std_logic;
 
    -- Clock period definitions
@@ -87,7 +60,7 @@ BEGIN
           clk_en => clk_en,
           reset_en => reset_en,
           spi_upload => spi_upload,
-			 state_reg => state_reg,
+	  state_reg => state_reg,
           out_StopCount => out_StopCount
         );
 
@@ -105,13 +78,13 @@ BEGIN
    stim_proc: process
    begin		
       
-      wait for 20.1 ms;	
-      sw <= '0';
+                wait for 20.1 ms;	
+                sw <= '0';
 		
 		-- insert stimulus here 
-      wait for 20.1 ms;
-      sw <= '1';
-      wait for 20.1 ms;
+                wait for 20.1 ms;
+                sw <= '1';
+                wait for 20.1 ms;
 		wait for 20.1 ms;
 		wait for 20.1 ms;
 		wait for 20.1 ms;

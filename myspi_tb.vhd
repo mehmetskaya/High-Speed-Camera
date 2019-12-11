ef8c367e8114481c7b65d0132524c39d4ebcd060
Mehmet S Kaya <msirackaya@gmail.com>
2019-12-11 08:54:38 -0700

    Update myspi_tb.vhd

--- myspi_tb.vhd
+++ myspi_tb.vhd
@@ -1,4 +1,3 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
  
@@ -18,15 +17,15 @@ architecture Behavioral of myspi_tb is
          clk : IN  std_logic;
          sck : IN  std_logic;
          ss : IN  std_logic_vector(1 downto 0);
-         reset_n : IN  std_logic;
+         reset : IN  std_logic;
          sck_hi : OUT  std_logic;
          tx_wr : IN  std_logic;
-         tx_di : IN  std_logic_vector(7 downto 0);
+         tx_d : IN  std_logic_vector(7 downto 0);
          miso : OUT  std_logic;
          tx_empty : OUT  std_logic;
          rx_rd : IN  std_logic;
          mosi : IN  std_logic;
-         rx_do : OUT  std_logic_vector(7 downto 0);
+         rx_d : OUT  std_logic_vector(7 downto 0);
          rx_full : OUT  std_logic
         );
     end component;
@@ -36,9 +35,9 @@ architecture Behavioral of myspi_tb is
    signal clk : std_logic;
    signal sck : std_logic;
    signal ss : std_logic_vector(1 downto 0);
-   signal reset_n : std_logic;
+   signal reset : std_logic;
    signal tx_wr : std_logic ;
-   signal tx_di : std_logic_vector(7 downto 0);
+   signal tx_d : std_logic_vector(7 downto 0);
    signal rx_rd : std_logic ;
    signal mosi : std_logic ;
    signal di : std_logic_vector(7 downto 0);
@@ -47,7 +46,7 @@ architecture Behavioral of myspi_tb is
    signal sck_hi : std_logic;
    signal miso : std_logic;
    signal tx_empty : std_logic;
-   signal rx_do : std_logic_vector(7 downto 0);
+   signal rx_d : std_logic_vector(7 downto 0);
    signal rx_full : std_logic;
 
    -- Clock period definitions
@@ -61,15 +60,15 @@ begin
           clk => clk,
           sck => sck,
           ss => ss,
-          reset_n => reset_n,
+          reset => reset,
           sck_hi => sck_hi,
           tx_wr => tx_wr,
-          tx_di => tx_di,
+          tx_d => tx_d,
           miso => miso,
           tx_empty => tx_empty,
           rx_rd => rx_rd,
           mosi => mosi,
-          rx_do => rx_do,
+          rx_d => rx_d,
           rx_full => rx_full
         );
 
@@ -87,13 +86,13 @@ begin
    stim_proc_rising: process
    begin	 
       wait for sck_period;
-      reset_n <= '0';
+      reset <= '0';
       sck<='0';
       wait for sck_period;
-      reset_n <= '1';
+      reset <= '1';
       
       wait for sck_period;
-      reset_n <= '0';
+      reset <= '0';
       wait for sck_period;
       ss <= "00";
       wait for sck_period;
@@ -110,7 +109,7 @@ begin
       end loop;
       rx_rd <= '1';
       tx_wr <= '1';
-      tx_di <= "11101001";
+      tx_d <= "10000110";
       mosi <= '0';
       wait for clk_period;
       rx_rd <= '0';
@@ -118,7 +117,7 @@ begin
       wait for sck_period;
             
       wait for sck_period;
-      di <= "10011001";
+      di <= "00000011";
       wait for sck_period;
       for i in 1 to 8 loop
         mosi <= di(8-i); 
@@ -128,8 +127,8 @@ begin
         sck<='0';
       end loop;
       rx_rd <= '0';
-      tx_wr <= '0';
-      tx_di <= "10001111";
+      tx_wr <= '1';
+      tx_d <= "10001111";
       mosi <= '0';
       wait for clk_period;
       rx_rd <= '0';
@@ -147,8 +146,8 @@ begin
         sck<='0';
       end loop;
       rx_rd <= '0';
-      tx_wr <= '1';
-      tx_di <= "11001011";
+      tx_wr <= '0';
+      tx_d <= "11001011";
       mosi <= '0';
       wait for clk_period;
       rx_rd <= '0';
@@ -167,7 +166,7 @@ begin
       end loop;
       rx_rd <= '1';
       tx_wr <= '0';
-      tx_di <= "10010111";
+      tx_d <= "10010111";
       mosi <= '0';
       wait for clk_period;
       rx_rd <= '0';
@@ -179,7 +178,7 @@ begin
       ss <= "00";
       wait for sck_period;
       
-      reset_n <= '1';
+      reset <= '1';
       wait for sck_period;
       assert false
       report "Simulation Ended"

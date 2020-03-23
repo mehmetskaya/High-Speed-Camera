e0c83ce538a82a7dd5c32fa29c1476f41599110c
Mehmet S Kaya <msirackaya@gmail.com>
2020-03-23 15:01:13 -0600

    Update spi_tb.vhd

--- spi_tb.vhd
+++ spi_tb.vhd
@@ -1,10 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
+
 entity spi_tb is
 end spi_tb;
  
